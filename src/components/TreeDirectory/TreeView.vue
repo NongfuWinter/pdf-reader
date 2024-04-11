ba3600e4b254e056f89b8e37ceccdd40548c0759
NongfuWinter <waitAParty@proton.me>
2024-04-11 23:47:23 +0800

    modified:   src/components/TreeDirectory/TreeView.vue

--- src/components/TreeDirectory/TreeView.vue
+++ src/components/TreeDirectory/TreeView.vue
@@ -9,20 +9,31 @@ const props = defineProps({
   },
 })
 
+enum OPERATION_MENU{
+  MAIN = 'main',
+  DRAG = 'drag',
+  ADD = 'add',
+}
+
 let treeCommuni = inject('treeCommuni') as Communication
 
 let selfView = ref<HTMLElement | null>(null)
 let titleView = ref<HTMLElement | null>(null)
 let scapegoatView = ref<HTMLElement | null>(null)
 let leavesView = ref<HTMLElement | null>(null)
+
 let isLeavesOpen = ref(false)
 let isBeChoosed = ref(false)
 let isDraging = ref(false)
 let isHover = ref(false)
 let isHoverLeaf = ref(false)
+let isDragEnter = ref(false)
 
+let dragEnterNumber = 0 
 let dragDiffX = 0
 let dragDiffY = 0
+
+let operationMenu = ref(OPERATION_MENU.MAIN)
 
 function titleContextmenu(event: any){
   if(isBeChoosed.value){
@@ -31,13 +42,12 @@ function titleContextmenu(event: any){
     isBeChoosed.value = true
     treeCommuni.setChooseId(props.tree.id, ()=>{isBeChoosed.value=false})
   }
-  
 }
 
 function dragOver(event: any){
   console.log('draging!');
   scapegoatView.value!.style.top = event.clientY + dragDiffY +'px'
-  scapegoatView.value!.style.left = event.clientX + dragDiffX +'px'
+  scapegoatView.value!.style.left = event.clientX +'px'
 }
 
 function dragStart(event: any){
@@ -52,11 +62,10 @@ function dragStart(event: any){
   isDraging.value = true
   nextTick(()=>{
     scapegoatView.value!.style.height = titleView.value!.clientHeight+'px'
-    scapegoatView.value!.style.width = selfView.value!.clientWidth+'px'
+    scapegoatView.value!.style.width = selfView.value!.clientWidth+dragDiffX+'px'
     scapegoatView.value!.style.visibility = 'visible'
     scapegoatView.value!.style.top = selfView.value!.getBoundingClientRect().top +'px'
-    scapegoatView.value!.style.left = selfView.value!.getBoundingClientRect().left +'px'
-
+    scapegoatView.value!.style.left = selfView.value!.getBoundingClientRect().left-dragDiffX +'px'
   })
   
   document.addEventListener('dragover', dragOver)
@@ -86,17 +95,27 @@ function dragEnd(event: any){
 }
 
 function dragEnter(event: any){
-  selfView.value!.classList.add('root-choosed')
-  console.log('enter');
+  isDragEnter.value = true
+  operationMenu.value = OPERATION_MENU.DRAG
   
+  console.log('enter: ', props.tree.content, event.target);
+  dragEnterNumber++
+
 }
 
 function dragLeave(event: any){
-  selfView.value!.classList.remove('root-choosed')
+  console.log('leave: ', props.tree.content, event.target);
+  dragEnterNumber--
+  if(dragEnterNumber == 0){
+    isDragEnter.value = false
+  }
 }
 
-function mouseenter(event: MouseEvent){
-  console.log(props.tree.content, event.currentTarget, event);
+function mouseOver(event: MouseEvent){
+  console.log('mouseOver', props.tree.content, event.currentTarget, event);
+  if(isDraging.value){
+    return
+  }
   
   if(event.target instanceof Node && leavesView?.value?.contains(event.target)){
     isHoverLeaf.value = true
@@ -111,10 +130,10 @@ function mouseleave(event: any){
   isHoverLeaf.value = false
 }
 
-
-
 function contentClick(){
-  isLeavesOpen.value = !isLeavesOpen.value
+  if(props.tree.leaves){
+    isLeavesOpen.value = !isLeavesOpen.value
+  }
 
 }
 
@@ -125,36 +144,76 @@ onMounted(()=>{
 
 <template>
   <div class="root" ref="selfView" 
-  :class="{'root-choosed':isBeChoosed, 'root-hover-bgc':isHover, 
-  'root-hover-boeder':isHover && !isHoverLeaf}" 
-  @dragenter="dragEnter($event)" @dragleave="dragLeave($event)" 
-  @mouseover="mouseenter($event)" @mouseout="mouseleave($event)">
+  :class="{'root-choosed': isBeChoosed || isDragEnter, 'root-hover-bgc': isHover,
+  'root-hover-boeder': isHover && !isHoverLeaf}" 
+  @dragenter.stop="dragEnter($event)" @dragleave.stop="dragLeave($event)" 
+  @mouseover="mouseOver($event)" @mouseout="mouseleave($event)">
     <div class="title" ref="titleView" :class="{'title-choosed':isBeChoosed}">
       <div class="content"
       @click="contentClick()"
       @contextmenu.prevent="titleContextmenu($event)"
       >
         <p>{{ props.tree.content }}</p>
-        <i class="bi bi-chevron-right" :class="{'i-tramsition': isLeavesOpen}"
-        v-if="props.tree.leaves != null"></i>
+        <i v-if="props.tree.leaves != null" 
+        class="bi bi-chevron-right" :class="{'i-transform': isLeavesOpen}"></i>
       </div>
+<Transition name="slide-fade">
+      <div class="operation" v-if="isBeChoosed || isDragEnter">
+        
+          <template v-if="operationMenu==OPERATION_MENU.MAIN">
+            <div draggable="true" @dragstart="dragStart($event)" @dragend="dragEnd($event)">
+              <i class="bi bi-grip-horizontal" style="color: #333;"></i>
+            </div>
+            <div>
+              <i class="bi bi-pencil-square" style="color: #25d;" ></i>
+            </div>
+            <div>
+              <i class="bi bi-plus-square" style="color: #2a0;" @click="operationMenu = OPERATION_MENU.ADD"></i>
+            </div>
+            <div>
+              <i class="bi bi-x-square" style="color: #d20;" @click="operationMenu = OPERATION_MENU.DRAG"></i>
+            </div>
+          </template>
+          <template v-else-if="operationMenu==OPERATION_MENU.DRAG">
+            <div>
+              <i class="bi bi-indent" style="transform: rotateZ(-90deg);color: #25d;"></i>
+            </div>
+            <div>
+              <i class="bi bi-fullscreen"></i>
+            </div>
+            <div>
+              <i class="bi bi-indent" style="transform: rotateZ(90deg);color: #25d;"></i>
+            </div>
+            <div>
+              <i class="bi bi-reply" @click="operationMenu = OPERATION_MENU.MAIN"></i>
+            </div>
+          </template>
 
-      <div class="operation" v-if="isBeChoosed">
-        <i class="bi bi-grip-horizontal" style="color: #333;"
-        draggable="true" @dragstart="dragStart($event)" @dragend="dragEnd($event)"></i>
-        <i class="bi bi-pencil-square" style="color: #25d;"></i>
-        <i class="bi bi-plus-square" style="color: #2a0;"></i>
-        <i class="bi bi-x-square" style="color: #d20;"></i>
-      </div>
+          <template v-else-if="operationMenu==OPERATION_MENU.ADD">
+            <div>
+              <i class="bi bi-indent"></i>
+            </div>
+            <div>
+              <i class="bi bi-reply " @click="operationMenu = OPERATION_MENU.MAIN"></i>
+            </div>
+          </template>
+        
+        
+
+        
+      </div></Transition>
+
     </div>
-    <div class="leaves" ref="leavesView" :class="{ open: isLeavesOpen }">
+    <div class="leaves" ref="leavesView" :class="{ 'leaves-open': isLeavesOpen }">
       <div>
         <template  v-for="leaf in props.tree">
           <TreeView v-if="leaf != null" :tree="leaf"></TreeView>
         </template>
       </div>
     </div>
-    <div  class="line" :class="{'line-open': isLeavesOpen}"><div></div></div>
+    <div class="line" :class="{'line-open': isLeavesOpen, 'line-choosed': isBeChoosed}">
+      <div></div>
+    </div>
   </div>
 
   <Teleport to="body">
@@ -162,7 +221,8 @@ onMounted(()=>{
     <div class="title title-choosed">
       <div class="content">
         <p>{{ props.tree.content }}</p>
-        <i v-if="props.tree.leaves != null" class="bi bi-chevron-right" :class="{'i-tramsition': isLeavesOpen}"
+        <i v-if="props.tree.leaves != null" class="bi bi-chevron-right" 
+        :class="{'i-tramsition': isLeavesOpen}"
         ></i>
       </div>
     </div>
@@ -171,13 +231,21 @@ onMounted(()=>{
 </template>
 
 <style scoped lang="less">
-.a{
-  position: fixed;
-  visibility: hidden;
-  background-color: rgba(221,221,221, 0.8);
-  pointer-events: none;
-  border: #bbb solid 1px;
+.slide-fade-enter-active {
+  transition: all .3s ease-in;
 }
+
+.slide-fade-leave-active {
+  transition: all .3s ease-in;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(200px);
+  opacity: 0;
+}
+
+@line-width: 2px;
 
 .root, .a {
   border-radius: 1rem;
@@ -185,6 +253,15 @@ onMounted(()=>{
   transition-property: background-color, border ;
   transition-duration: .2s, .5s;
   border: rgba(0, 0, 0, 0) solid 2px;
+}
+
+.a{
+  position: fixed;
+  visibility: hidden;
+  background-color: rgba(221,221,221, 0.8);
+  border: #ccc solid 2px;
+  padding-left: @line-width;
+  pointer-events: none;
 }
 
 .root{
@@ -207,6 +284,7 @@ onMounted(()=>{
 
 .root-choosed {
   background-color: #eee;
+  
   border: #ddd solid 2px;
 }
 
@@ -225,25 +303,34 @@ onMounted(()=>{
     display: flex;
     flex-grow: 1;
     align-items: center;
-    padding: 0.6rem 1rem;
+    padding: .6rem 1rem;
     p {
       vertical-align: middle;
       margin: 0 1.5rem 0 0;
+      user-select: none;
     }
     i{
       transition: transform .5s;
     }
-    .i-tramsition{
+    .i-transform{
       transform: rotateZ(90deg);
     }
   }
 
   .operation {
-    padding: 0.6rem 1rem;
-    &>i{
-      margin-right: 1rem;
+    display: flex;
+    align-self: stretch;
+    padding: auto 1rem;
+
+    div{
+      display: flex;
+      margin-right: 1.4rem;
+      align-items: center;
     }
-    i:last-child{
+    & i{
+      font-size: 1.1rem;
+    }
+    & i:last-child{
       margin-right: 0;
     }
   }
@@ -267,7 +354,7 @@ onMounted(()=>{
   }
 }
 
-.open {
+.leaves-open {
   grid-template-rows: 1fr;
 }
 
@@ -277,7 +364,8 @@ onMounted(()=>{
   grid-template-rows: 0fr;
   transition: all .3s ease-in;
   margin-top: 1rem;
-  width: 2px;
+  margin-left: .5rem;
+  width: @line-width;
   &>div{
     background-color: #aca;
     min-height: 0;
@@ -286,5 +374,9 @@ onMounted(()=>{
 
 .line-open{
   grid-template-rows: 1fr;
+}
+
+.line-choosed > div{
+  background-color: #aae;
 }
 </style>
